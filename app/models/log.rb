d67f507f2d3c211ad6caed0ad80175f92602fc12
Amy Westcott <amy.westcott@yahoo.com>
2020-02-28 14:43:26 -0700

    set up relationships in models

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,4 @@
 class Log < ApplicationRecord
+    belongs_to :routes
+    has_many :comments
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,7 @@
 class Route < ApplicationRecord
     validates :user_id, presence: true
+    has_many :logs
+    has_many :comments through :logs
     belongs_to :user 
 
     scope :search, -> (query, user) { query ? user.routes.where("name LIKE ?", "%#{query}%") : user.routes }
